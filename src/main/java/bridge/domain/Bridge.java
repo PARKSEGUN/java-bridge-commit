9a0f1f5bf8cb21c7a64caaa2db4324f1781145f1
박세건 <90709751+PARKSEGUN@users.noreply.github.com>
2023-11-25 21:49:27 +0900

    refactor: 다리 생성시 검증 위치 수정

--- src/main/java/bridge/domain/Bridge.java
+++ src/main/java/bridge/domain/Bridge.java
@@ -1,12 +1,14 @@
 package bridge.domain;
 
 import bridge.constant.MovingCommand;
+import bridge.validator.BridgeValidator;
 import java.util.List;
 
 public class Bridge {
     private final List<String> bridgeInfo;
 
     public Bridge(List<String> bridgeInfo) {
+        BridgeValidator.validateSize(bridgeInfo.size());
         this.bridgeInfo = bridgeInfo;
     }
 
--- src/main/java/bridge/domain/BridgeMaker.java
+++ src/main/java/bridge/domain/BridgeMaker.java
@@ -5,7 +5,6 @@ import static bridge.constant.BridgeConstant.BRIDGE_UP_POSITION;
 import static bridge.constant.BridgeConstant.BRIDGE_UP_SIGN;
 
 import bridge.BridgeNumberGenerator;
-import bridge.validator.BridgeValidator;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -25,7 +24,6 @@ public class BridgeMaker {
      * @return 입력받은 길이에 해당하는 다리 모양. 위 칸이면 "U", 아래 칸이면 "D"로 표현해야 한다.
      */
     public List<String> makeBridge(int size) {
-        BridgeValidator.validateSize(size);
         List<String> bridgeInfo = new ArrayList<>();
         for (int i = 0; i < size; i++) {
             int bridgeNumber = bridgeNumberGenerator.generate();
