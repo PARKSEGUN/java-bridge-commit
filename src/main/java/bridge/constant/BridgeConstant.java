026a4c6ab5c2b5435294e0b3923eb76c93f9de42
박세건 <90709751+PARKSEGUN@users.noreply.github.com>
2023-11-26 22:13:29 +0900

    style: 개행 제거

--- src/main/java/bridge/constant/BridgeConstant.java
+++ src/main/java/bridge/constant/BridgeConstant.java
@@ -3,12 +3,9 @@ package bridge.constant;
 public final class BridgeConstant {
     public static final int BRIDGE_MAX_SIZE = 20;
     public static final int BRIDGE_MIN_SIZE = 3;
-
     public static final int BRIDGE_UP_POSITION = 1;
     public static final int BRIDGE_DOWN_POSITION = 0;
-
     public static final String BRIDGE_UP_SIGN = "U";
-
     public static final String BRIDGE_DOWN_SIGN = "D";
     public static final int BRIDGE_FLOOR_SIZE = 2;
 
